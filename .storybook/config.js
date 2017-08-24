740f88bb0b364298f247b88d810c481fc61e9109
Luca Barone <baro.luc@gmail.com>
2017-08-25 00:25:04 +0200

    :tada: storybook init

---
+++ .storybook/config.js
@@ -0,0 +1,8 @@
+import 'sanitize.css'
+import {configure} from '@storybook/react'
+
+const req = require.context('../source', true, /\.stories\.js$/)
+
+const loadStories = () => req.keys().forEach(req)
+
+configure(loadStories, module)
---
+++ source/react-inspect.stories.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {storiesOf} from '@storybook/react'
+
+storiesOf('Inspect', module)
+  .add('default', () => (
+    <button>Wooo</button>
+  ))
